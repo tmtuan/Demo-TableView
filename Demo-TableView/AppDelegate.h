59f916417a6fe96ebea4557e415f0042533e6667
mtuan <tranminhtuan684@gmail.com>
2017-03-08 08:28:38 +0700

    Initial Demo TableView Project

---
+++ Demo-TableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo-TableView
+//
+//  Created by ThaoLT on 3/8/17.
+//  Copyright © 2017 tmtuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
