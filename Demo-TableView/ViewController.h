d84269d13506265cdcdf770ce2bcca1027cd6d15
mtuan <tranminhtuan684@gmail.com>
2017-03-08 08:40:57 +0700

    Replace View by TableView on Storyboard, connect TableView to property, Syntherize property, connect dataSource and delegate to ViewController

--- Demo-TableView/ViewController.h
+++ Demo-TableView/ViewController.h
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UITableViewController
 
-
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
 @end
 
--- Demo-TableView/ViewController.m
+++ Demo-TableView/ViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ViewController
 
+@synthesize tableView = _tableView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
